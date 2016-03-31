07284a371e3054f400537c1c744b2f13b6ca9200
Ethan Hart <ethan.john.hart@gmail.com>
2016-03-30 18:04:55 -0700

    Add a script for some CG analysis

--- queries/cg.sql
+++ queries/cg.sql
@@ -1,13 +1,14 @@
  # Get complete game stats
  # Include IP, SO9 (k/9), others
- 
+
  SELECT playerID, yearID, teamID,
-     SO, CG, IPOuts, W, L, GS,
+     ERA, CG, SO, SHO, IPOuts, W, L, GS,
      CG / GS AS CGperGS,
      IPOuts/3 AS IP,
      SO / (IPOuts/3/9) AS SO9,
-     (IPOuts/3) / GS AS IperGS
+     (IPOuts/3) / GS AS IPperGS,
+     ((13*HR)+(3*(BB+HBP))-(2*SO))/(IPOuts/3) + 3.10 AS FIP
  FROM Pitching
- WHERE yearID > 2000
- HAVING CG > 1 AND GS > 5
- ORDER BY CGperGS DESC
+ WHERE yearID > 1980
+ HAVING GS > 10
+ ORDER BY FIP ASC
