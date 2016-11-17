c03725860d07765803457b2255f6e588e775cdd8
Ethan Hart <ethan.john.hart@gmail.com>
2016-11-16 16:37:15 -0800

    Add some comments, fix 'while' loop

--- scripts/ab4ba.py
+++ scripts/ab4ba.py
@@ -6,20 +6,19 @@
 
 from sys import argv
 
-ba = float(argv[1])
-ab = 1
-n = 5
+ba = float(argv[1])  # 0 < ba > 1.000
+n = 5  # number of results
 answers = []
-print 'To achieve a BA of {0}:'.format(ba)
-while True:
-    for i in range(ab):
+ab = 1
+while len(answers) < n:  # test until we have n answers
+    for i in range(ab):  # for each ab, test each case (e.g. 1/3, 2/3, 3/3)
         hits = i + 1
-        tmp_ba = round(hits / float(ab), 3)
+        tmp_ba = round(hits / float(ab), 3)  # batting average to check
         if tmp_ba == ba:
             answers.append('{0} hits needed in {1} ABs'.format(hits, ab))
-            if len(answers) == n:
-                for ans in answers:
-                    print ans
-                exit()
-            break
     ab += 1
+
+# print results
+print 'To achieve a BA of {0}:'.format(ba)
+for ans in answers:
+    print ans
